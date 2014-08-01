9b1facc3d2d70ca6bb50128a2b9f5f3351ab77a7
Matheus Lopes Santos <fale_com_lopez@hotmail.com>
2014-08-01 11:28:40 -0300

    Banco de Dados
    Redefinição das configurações do banco de dados em
    *./application/config/database.php*

--- application/config/database.php
+++ application/config/database.php
@@ -50,7 +50,7 @@ $active_record = TRUE;
 
 $db['default']['hostname'] = 'localhost';
 $db['default']['username'] = 'root';
-$db['default']['password'] = '';
+$db['default']['password'] = 'JeSuS101';
 $db['default']['database'] = 'pent_pentaurea';
 $db['default']['dbdriver'] = 'mysql';
 $db['default']['dbprefix'] = '';
